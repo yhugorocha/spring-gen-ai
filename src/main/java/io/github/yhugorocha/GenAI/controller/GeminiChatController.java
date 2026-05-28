a7bd97e6b53c15cb67c7dd1b8d163ef4d8b45a98
Hugo Silva <jose.silva@extreme.digital>
2026-05-28 11:27:44 -0300

    feat: update SYSTEM_PROMPT and user instructions to provide guidance in Portuguese

--- src/main/java/io/github/yhugorocha/GenAI/controller/GeminiChatController.java
+++ src/main/java/io/github/yhugorocha/GenAI/controller/GeminiChatController.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GeminiChatController {
 
     private final ChatClient chatClient;
-    private final static String SYSTEM_PROMPT = "You are a helpful assistant that summarizes meeting notes.";
+    private final static String SYSTEM_PROMPT = """
+            Você é um assistente prestativo que resume qualquer conteúdo fornecido.
+            Certifique-se de que o resumo seja conciso, informativo e capture os pontos principais.
+            Use um tom amigável e acessível, mantendo o profissionalismo.
+            """;
 
     public GeminiChatController(@Qualifier("geminiChatClient") ChatClient chatClient) {
         this.chatClient = chatClient;
@@ -23,8 +27,23 @@ public class GeminiChatController {
 
         return chatClient.prompt()
                 .system(SYSTEM_PROMPT)
-                .user(u -> u.text("Here are the meeting notes: {meetingNotes} Please provide a " +
-                        "concise summary of the key points and action items.")
+                .user(u -> u.text("""
+                                Você pode resumir as seguintes anotações da reunião: {meetingNotes}
+                                Use o formato descrito no exemplo a seguir ao fazer o resumo:
+                                Entrada:
+                                Na reunião de estratégia de vendas de hoje, revisamos as metas do terceiro trimestre e as lacunas de desempenho.
+                                A equipe concordou em focar em clientes corporativos e fortalecer parcerias.
+                                Foi feita uma proposta para expandir para duas novas regiões.
+                                O marketing sugeriu alinhar as campanhas com os objetivos de vendas para melhorar a conversão de leads e encurtar os ciclos de vendas.
+                                Saída:
+                                Itens de ação:
+                                * Focar em clientes corporativos e parcerias.
+                                * Explorar a expansão para duas novas regiões.
+                                * Alinhar as campanhas de marketing com os objetivos de vendas.
+                                Decisões:
+                                * Clientes corporativos priorizados para o terceiro trimestre.
+                                * Marketing e vendas trabalharão em conjunto na conversão de leads.
+                                """)
                         .param("meetingNotes", meetingNotes))
                 .call()
                 .content();
